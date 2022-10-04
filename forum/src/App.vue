93600fca4533f1edf903d9bb198dd683c4ceb619
zzzzz <zolboo0906@gmail.com>
2022-10-04 09:34:44 +0800

    Lesson 14
    Дэлгэцэнд мэдээлэл жагсаалтаар харуулах. 1-р хэсэг.
    https://v2.vuejs.org/v2/style-guide/#Single-file-component-filename-casing-strongly-recommended

--- forum/src/App.vue
+++ forum/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld />
+  <PageHome />
 
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PageHome from './components/PageHome.vue'
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PageHome
   }
 }
 </script>
---
+++ forum/src/components/PageHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Форумд тавтай морилно уу!</h2>
+  </div>
+  <div v-for="thread in threads" :key="thread.id"> {{thread.title}}</div>
+
+</template>
+<script>
+import forumData from '@/forumData.json'
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      threads: forumData.threads
+    }
+  }
+
+}
+</script>
+<style>
+
+</style>
